d6f6724b98a9b5bbaa41d317687829496f28db4d
hellomarfil <154944875+hellomarfil@users.noreply.github.com>
2023-12-26 21:42:15 -0600

    Delete public_html directory

--- public_html/wp-content/plugins/podamibe-simple-footer-widget-area/assets/js/custom-script.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(document).ready(function () {
-    // Add Color Picker to all inputs that have 'color-field' class
-    jQuery(function () {
-        jQuery('.color-field').wpColorPicker();
-    });
-
-});
